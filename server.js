f814852af9899f1359b6c7d62aa516ee27436442
MohamedAyman98 <mohamedayman98@github.com>
2019-11-22 18:19:02 +0200

    Some updates

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//Db config
+const db = require("./config/keys").mongoURI;
+
+//Connect to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB.."))
+  .catch(error => console.log(error));
 
 const port = process.env.PORT || 5000;
 
