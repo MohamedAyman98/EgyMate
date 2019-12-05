09956e70ab8e67d60bc0c2035ea2d98820a29196
MohamedTarawa <mohamed.tarawa@student.guc.edu.eg>
2019-12-05 21:29:01 +0200

    Landing Page Component

---
+++ client/src/components/Landing.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import LandingNavbar from "./LandingNavBar";
+import { Button, ButtonToolbar } from "react-bootstrap";
+
+export class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <LandingNavbar />
+        <header class="masthead">
+          <div class="container h-100">
+            <div class="row h-100 align-items-center">
+              <div class="col-12 text-center">
+                <h1
+                  class="font-weight-light"
+                  style={{
+                    color: "white",
+                    textTransform: "uppercase",
+                    fontSize: "70px",
+                    textAlign: "center"
+                  }}
+                >
+                  <b>Egy Mate</b>
+                </h1>
+                <p class="lead">A great facility for your trips</p>
+              </div>
+            </div>
+          </div>
+        </header>
+        
+        <section class="py-5">
+          <div class="container">
+            <h2 class="font-weight-light">Welcome !</h2>
+            <p>
+              EgyMate is a website for both tourists and tour guides. We
+              offer a unique service that matches tourists with independant tour
+              guides, removing the stress of having to plan your own trip when
+              visiting a new place in Egypt.{" "}
+              <b>
+                To join, Please Register or Login.
+              </b>
+            </p>
+            <ButtonToolbar>
+              <Button variant="info">
+                Register
+              </Button>
+              <Button
+                href="/Tourist"
+                variant="info"
+                style={{ marginLeft: "7px" }}
+              >
+                Login
+              </Button>
+            </ButtonToolbar>
+          </div>
+        </section>
+        
+      </div>
+    );
+  }
+}
+
+export default Landing;
