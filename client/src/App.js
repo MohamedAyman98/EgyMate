13d8ae6f3af280cc7a85ed7415587cfe9f07f320
MohamedTarawa <mohamed.tarawa@student.guc.edu.eg>
2019-12-09 19:14:43 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import TouristSelectTrip from "./components/TouristSelectTrip";
 import Landing from "./components/Landing";
 import Signin from "./components/Signin";
+import MakeTrip from "./components/MakeTrip";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route exact path="/" component={Landing} />
       <Route exact path="/selectTrip" component={TouristSelectTrip} />
       <Route exact path="/signin" component={Signin} />
+      <Route exact path="/maketrip" component={MakeTrip} />
     </BrowserRouter>
   );
 }
